14098519c91568ed535406cc1bed9be130ef0a10
Stephane Mouako <stephane.mouako@sioux.eu>
2024-10-27 03:40:21 +0100

    Added functinalites to export and import data

--- SampleEmployeeManagementSystem/SampleEmployeeManagementSystem/DataAccess/Services/EmployeeService.cs
+++ SampleEmployeeManagementSystem/SampleEmployeeManagementSystem/DataAccess/Services/EmployeeService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json;
+using ClosedXML.Excel;
+using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
 using SampleEmployeeManagementSystem.DataAccess.Data;
 using SampleEmployeeManagementSystem.DataAccess.Entities;
 using SampleEmployeeManagementSystem.ViewModels;
@@ -89,5 +92,56 @@ public class EmployeeService(AppDbContext appDbContext)
         {
             return false;
         }
+    }
+
+    public async Task<bool> ImportEmployeeAsync(List<EmployeeViewModel> employees)
+    {
+        try
+        {
+            var employeeEntities = employees.Select(e => new Employee
+            {
+                FullName = e.FullName,
+                Departement = e.Departement,
+                DateOfBirth = e.DateOfBirth,
+                Age = e.Age,
+                PhoneNumber = e.PhoneNumber
+            }).ToList();
+            await appDbContext.BulkInsertAsync(employeeEntities);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public async Task<byte[]> ExportEmployeesToExcelAsync()
+    {
+        var employees = await GetAllEmployeesAsync();
+        using var workbook = new XLWorkbook();
+        var worksheet = workbook.Worksheets.Add("Employees");
+
+        // Header
+        worksheet.Cell(1, 1).Value = "Employee Id";
+        worksheet.Cell(1, 2).Value = "Full Name";
+        worksheet.Cell(1, 3).Value = "Departement";
+        worksheet.Cell(1, 4).Value = "Date Of Birth";
+        worksheet.Cell(1, 5).Value = "Age";
+        worksheet.Cell(1, 6).Value = "Phone Number";
+
+        // Data
+        for (var i = 0; i < employees.Count; i++)
+        {
+            worksheet.Cell(i + 2, 1).Value = employees[i].Id;
+            worksheet.Cell(i + 2, 2).Value = employees[i].FullName;
+            worksheet.Cell(i + 2, 3).Value = employees[i].Departement;
+            worksheet.Cell(i + 2, 4).Value = employees[i].DateOfBirth;
+            worksheet.Cell(i + 2, 5).Value = employees[i].Age;
+            worksheet.Cell(i + 2, 6).Value = employees[i].PhoneNumber;
+        }
+
+        using var stream = new MemoryStream();
+        workbook.SaveAs(stream);
+        return stream.ToArray();
     }
 }
